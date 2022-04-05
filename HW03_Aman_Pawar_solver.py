3e3f81533d58cce94cf1986bf08890c9a8cfe2ab
Aman Pawar <amanpawar507@gmail.com>
2022-04-04 21:50:09 -0400

    Wordle solver implemented

---
+++ HW03_Aman_Pawar_solver.py
@@ -0,0 +1,88 @@
+import HW03_Aman_Pawar_helper as helper
+import HW03_Aman_Pawar_wordle as wordle
+import random
+
+class Solver:
+
+    words = []
+    def __init__(self):
+        self.words = []
+        self.myMain = wordle.Main()
+        self.helps = helper.Helper()
+
+    def convert(self, s):
+        # initialization of string to ""
+        str1 = ""
+    
+        # using join function join the list s by 
+        # separating words by str1
+        return(str1.join(s))
+      
+    def mySolver(self):  
+
+        f = open("filter_list_file.txt")
+        self.words = f.read().split("\n")
+        solution = list(self.words[random.randint(0, 1378)].upper())
+        myWordle = self.convert(solution).lower()
+        attempts = 0
+        #self.myMain.game(self.words, 0, solution)
+
+        goodLetters=[]
+        badLetters=[]
+        index0=""
+        index1=""
+        index2=""
+        index3=""
+        index4=""
+
+        initialGuess = "sales"
+        guesslist = []
+        print("The answer for this round is" ,myWordle)
+        print()
+
+        while attempts < 6:
+            print(f'Guess #{attempts+1}')
+            print("My guess is",initialGuess)
+            try:
+                result = self.myMain.solverHelper(myWordle.upper(), attempts, initialGuess.upper())
+            except:
+                print("Not found")
+
+            if result == "Wordle found":
+                print(f"You win! Your word is {initialGuess}")
+                break
+
+            for i in range(5):
+                if result[i] == " " and i == 0:
+                    index0 = initialGuess[i]
+                
+                elif result[i] == " " and i == 1:
+                    index1 = initialGuess[i]
+                
+                elif result[i] == " " and i == 2:
+                    index2 = initialGuess[i]
+                
+                elif result[i] == " " and i == 3:
+                    index3 = initialGuess[i]
+                
+                elif result[i] == " " and i == 4:
+                    index4 = initialGuess[i]
+
+                elif result[i] == "'":
+                    goodLetters.append(initialGuess[i])
+
+            for i in range(5):
+                if result[i] == '"' and initialGuess[i] not in goodLetters and initialGuess[i] != index0 and initialGuess[i] != index1 and initialGuess[i] != index2 and initialGuess[i] != index3 and initialGuess[i] != index4:
+                    badLetters.append(initialGuess[i])
+                    
+            attempts = attempts + 1
+            guesslist.append(initialGuess)
+            print()
+            initialGuess = self.helps.rankedWords(goodLetters,badLetters)
+            if initialGuess==None:
+                print("Failed to solve the Wordle \n")
+                break
+
+
+myRun = Solver()
+myRun.mySolver() 
--- HW03_Aman_Pawar_wordle.py
+++ HW03_Aman_Pawar_wordle.py
@@ -42,6 +42,115 @@ class Main:
             else:
                 return False
 
+    def solverHelper(self, myWord, x, initialGuess):
+        #variables for storing abbreviations
+        final_list = [None] * 5
+        final_string = ""
+        
+        #Taking input
+        words = [None]*6
+        words[x] = initialGuess
+        if words[x] == None:
+            quit()
+        
+        copy_myWord = myWord
+        #Check if the word was correct
+        if (self.compare(words[x].upper(), myWord)):
+            print("Correct guess, You win!")
+            self.gameStats[x] += 1
+            self.gamesWon += 1
+            return "Wordle found"
+        else:
+            print(words[x].upper())
+            #Looping to check each letter
+            for y in range(5):
+                #Check which letter is in correct position and is a correct word
+                if words[x][y].upper() == copy_myWord[y]:
+                    i = y
+                    copy_myWord =  copy_myWord[:i] +" "+ copy_myWord[i+1:]
+                    final_list[y] = " "
+                    
+            for y in range(5):   
+                if(final_list[y] != None):
+                    continue
+                else:
+                    #Check which letter is correct but in wrong position
+                    if words[x][y].upper() in copy_myWord:
+                        my_letter = words[x][y].upper()
+                        i= int(copy_myWord.find(my_letter))
+                        copy_myWord =  copy_myWord[:i] +" "+ copy_myWord[i+1:]
+                        final_list[y] = "`"
+                    
+                    #Check if the letter is incorrect  
+                    else: 
+                        final_list[y] = '"'
+                        continue
+            
+            for i in final_list:
+                final_string += str(i) + ""
+            print(final_string)
+
+            #Exit when all attempts are exhausted
+            if x==5: print("You lose!")
+            return final_string
+
+    def game(self, words, x, myWord):
+        #variables for storing abbreviations
+        final_list = [None] * 5
+        final_string = ""
+
+        #Creating copy of input words 
+        copy_words = words.copy()
+        
+        #Taking input
+        print("\nGuess the word")
+        
+        words[x] = self.u.get_input(copy_words, x)
+        if words[x] == None:
+            quit()
+        
+        copy_myWord = myWord
+        #Check if the word was correct
+        if (self.compare(words[x].upper(), myWord)):
+            print("Correct guess, You win!")
+            self.gameStats[x] += 1
+            self.gamesWon += 1
+            return "Wordle found"
+        else:
+            print(words[x].upper())
+            #Looping to check each letter
+            for y in range(5):
+                #Check which letter is in correct position and is a correct word
+                if words[x][y].upper() == copy_myWord[y]:
+                    i = y
+                    copy_myWord =  copy_myWord[:i] +" "+ copy_myWord[i+1:]
+                    final_list[y] = " "
+                    
+            for y in range(5):   
+                if(final_list[y] != None):
+                    continue
+                else:
+                    #Check which letter is correct but in wrong position
+                    if words[x][y].upper() in copy_myWord:
+                        my_letter = words[x][y].upper()
+                        i= int(copy_myWord.find(my_letter))
+                        copy_myWord =  copy_myWord[:i] +" "+ copy_myWord[i+1:]
+                        final_list[y] = "`"
+                    
+                    #Check if the letter is incorrect  
+                    else: 
+                        final_list[y] = '"'
+                        continue
+            
+            for i in final_list:
+                final_string += str(i) + ""
+            print(final_string)
+
+            #Exit when all attempts are exhausted
+            if x==5: print("You lose!")
+            return final_string
+                
+
     def main(self):
         
         while(True):
@@ -62,61 +171,14 @@ class Main:
             if self.clear_list(used_words):
                 used_words.clear() 
             #Loop for 6 times for 6 attempts
+
             for x in range(6):
-                #variables for storing abbreviations
-                final_list = [None] * 5
-                final_string = ""
-
-                #Creating copy of input words 
-                copy_words = words.copy()
-                
-                #Taking input
-                print("\nGuess the word")
-                
-                words[x] = self.u.get_input(copy_words, x)
-                if words[x] == None:
-                    quit()
-                
-                copy_myWord = myWord
-                #Check if the word was correct
-                if (self.compare(words[x].upper(), myWord)):
-                    print("Correct guess, You win!")
-                    self.gameStats[x] += 1
-                    self.gamesWon += 1
-                    break
-                else:
-                    print(words[x].upper())
-                    #Looping to check each letter
-                    for y in range(5):
-                        #Check which letter is in correct position and is a correct word
-                        if words[x][y].upper() == copy_myWord[y]:
-                            i = y
-                            copy_myWord =  copy_myWord[:i] +" "+ copy_myWord[i+1:]
-                            final_list[y] = " "
-                            
-                    for y in range(5):   
-                        if(final_list[y] != None):
-                            continue
-                        else:
-                            #Check which letter is correct but in wrong position
-                            if words[x][y].upper() in copy_myWord:
-                                my_letter = words[x][y].upper()
-                                i= int(copy_myWord.find(my_letter))
-                                copy_myWord =  copy_myWord[:i] +" "+ copy_myWord[i+1:]
-                                final_list[y] = "`"
-                            
-                            #Check if the letter is incorrect  
-                            else: 
-                                final_list[y] = '"'
-                                continue
+                    stringReturned = self.game(words, x,myWord)
+                    if(stringReturned == "Wordle found"):
+                        break
+                    else:
+                        continue
                     
-                    for i in final_list:
-                        final_string += str(i) + ""
-                    print(final_string)
-
-                    #Exit when all attempts are exhausted
-                    if x==5: print("You lose!")
-                    continue    
             self.gamesPlayed+=1
             try:
                 self.output_stats(self.gamesPlayed, self.gamesWon, self.gameStats)
